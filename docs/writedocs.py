57c00403903dfc7f4b7fbbf0c8374197b24590b3
Coby Chapman <cobylc.121@gmail.com>
2020-09-12 17:54:57 +0100

    docs

--- docs/writedocs.py
+++ docs/writedocs.py
@@ -1,5 +1,2 @@
 import os
-
-for file_ in os.listdir("C:\\Users\\cobyl\\Documents\\Coding\\MicrosoftCode\\ActualDevelopment\\PythonQuickStartModule\\src"):
-    if file_.endswith(".py"):
-        os.system("python -m pdoc --html --force C:\\Users\\cobyl\\Documents\\Coding\\MicrosoftCode\\ActualDevelopment\\PythonQuickStartModule\\src\\{} -o C:\\Users\\cobyl\\Documents\\Coding\\MicrosoftCode\\ActualDevelopment\\PythonQuickStartModule\\docs".format(file_))
+os.system("python -m pdoc --html --force C:\\Users\\cobyl\\Documents\\Coding\\MicrosoftCode\\ActualDevelopment\\PythonQuickStartModule\\src -o C:\\Users\\cobyl\\Documents\\Coding\\MicrosoftCode\\ActualDevelopment\\PythonQuickStartModule\\docs")
